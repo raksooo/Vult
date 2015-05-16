38edff6dd5780b5fcde8606edb5bb59ac6037fab
Mike 'Anno' Phoohad <anno.tao@gmail.com>
2015-05-16 11:49:50 +0200

    added structure to app

--- scripts/app.js
+++ scripts/app.js
@@ -1,76 +1,101 @@
-var app = function(){
-	function searchMovie(){
-		SearchMoviesOnImdb()
-	};
+exports = {
+  init: app
 };
 
-function createXmlQuery(name, params){
-	var result = "<methodCall>" +
-    "<methodName>"+ name+ "</methodName>"; 
-    if (params>0) {
-    	result += "<params>";
-    	for (param of params){
-    		result += "<param>";
-    		if(param.type==="string"){
-    			result += "<value><string>"+param.value+"</string></value>";
-    		}
-    		result+="</param>";
-    	}; 
-    	result +="</params>";
-    };
-    result+="</methodCall>";
-    return result;
-};
-//Söka filmer, leta subtitles till filmer, ladda ner subtitles
-var parseString = require('xml2js').parseString;
-var xml = '<?xml version="1.0" encoding="UTF-8" ?>';
-parseString(xml, function (err, result) {
-    console.dir(JSON.stringify(result));
-});
+var app = function() {
+  var data = {
+    searchMovie: searchMovie,
+    init: init()
+  };
+  init();
 
-function login() {
-  var xmlData = "<methodCall>" +
-    "<methodName>LogIn</methodName>" +
-    "<params>" +
-    "<param>" +
-    "<value><string></string></value>" +
-    "</param>" +
-    "<param>" +
-    "<value><string></string></value>" +
-    "</param>" +
-    "<param>" +
-    "<value><string>en</string></value>" +
-    "</param>" +
-    "<param>" +
-    "<value><string>OS Test User Agent</string></value>" +
-    "</param>" +
-    "</params>" +
-    "</methodCall>";
-  var xmlTest = "<methodCall>" +
-    "<methodName>ServerInfo</methodName>" +
-    "</methodCall>";
-  console.log(xmlTest);
 
-  var postRequest = {
-    host: 'api.opensubtitles.org',
-    port: 80,
-    path: "/xml-rpc",
-    method: 'POST',
-    headers: {
-        'Content-Type': 'text/xml',
-        'Content-Length': Buffer.byteLength(xmlTest)
-    }
+  function init() {
+    login();
   };
 
-  var buffer = '';
-  var req = http.request( postRequest, function( res )    {
+	function searchMovie(){
+    SearchMoviesOnImdb();
+	}
+  //};
 
-    console.log(res);
-    console.log( res.statusCode );
-    buffer = "";
-    res.on( "data", function( data ) { buffer = buffer + data; } );
-    res.on( "end", function( data ) { console.log( buffer ); } );
+  function createXmlQuery(name, params){
+  	var result = "<methodCall>" +
+      "<methodName>"+ name+ "</methodName>";
+      if (params>0) {
+      	result += "<params>";
+      	for (var param in params){
+      		result += "<param>";
+      		if(param.type==="string"){
+      			result += "<value><string>"+param.value+"</string></value>";
+      		}
+      		result+="</param>";
+      	}
+      	result +="</params>";
+      }
+      result+="</methodCall>";
+      return result;
+  }
+  //Söka filmer, leta subtitles till filmer, ladda ner subtitles
+  var parseString = require('xml2js').parseString;
+  var xml = '<?xml version="1.0" encoding="UTF-8" ?>';
+  parseString(xml, function (err, result) {
+      console.dir(JSON.stringify(result));
   });
+  function login() {
+    var xmlData = "<methodCall>" +
+      "<methodName>LogIn</methodName>" +
+      "<params>" +
+      "<param>" +
+      "<value><string></string></value>" +
+      "</param>" +
+      "<param>" +
+      "<value><string></string></value>" +
+      "</param>" +
+      "<param>" +
+      "<value><string>en</string></value>" +
+      "</param>" +
+      "<param>" +
+      "<value><string>OSTestUserAgent</string></value>" +
+      "</param>" +
+      "</params>" +
+      "</methodCall>";
 
-  req.write(xmlTest);
-  req.end();
+    var xml = createXmlQuery("LogIn",
+      [{'type': 'string', 'value': ''},
+       {'type': 'string', 'value': ''},
+       {'type': 'string', 'value': ''},
+       {'type': 'string', 'value': ''},
+       {'type': 'string', 'value': 'OSTestUserAgent'}]
+     );
+    var xmlTest = "<methodCall>" +
+      "<methodName>ServerInfo</methodName>" +
+      "</methodCall>";
+    console.log(xml);
+
+    var postRequest = {
+      host: 'api.opensubtitles.org',
+      port: 80,
+      path: "/xml-rpc",
+      method: 'POST',
+      headers: {
+          'Content-Type': 'text/xml',
+          'Content-Length': Buffer.byteLength(xmlTest)
+      }
+    };
+
+    var buffer = '';
+    var req = http.request( postRequest, function( res )    {
+
+      console.log(res);
+      console.log( res.statusCode );
+      buffer = "";
+      res.on( "data", function( data ) { buffer = buffer + data; } );
+      res.on( "end", function( data ) { console.log( buffer ); } );
+    });
+
+    req.write(xml);
+    req.end();
+
+  }
+}
--- server.js
+++ server.js
@@ -17,3 +17,5 @@ var server = app.listen(8888, function () {
 
 });
 
+
+
