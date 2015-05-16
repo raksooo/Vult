c111a5dcdaa17a7b1932d7d19f1935bb93df5122
Mike 'Anno' Phoohad <anno.tao@gmail.com>
2015-05-16 08:31:21 +0200

    Configured quick server and added package.json

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-var express = require('express'),
-    mysql = require('mysql');
-    app = express();
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Server up and running', host, port);
+
+});
