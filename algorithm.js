ccec8b2b9bea3742efeafa9c9a48c602d1a51fff
Oskar Nyberg <oskar@oskarnyberg.com>
2015-05-16 13:06:58 +0200

    Finnished algorithm

--- algorithm.js
+++ algorithm.js
@@ -1,13 +1,71 @@
 var fs = require('fs');
 
 var DATE = '1970-01-01';
+var BITS_PER_POSITION = 14;
 
-fs.readFile(__dirname + "/example_subtitle/exMachina_short.srt", function(err, sub) {
-    console.log(toBinary(parseSubtitle(sub), 1));
+fs.readFile(__dirname + "/example_subtitle/exMachina.srt", function(err, sub1) {
+    fs.readFile(__dirname + "/example_subtitle/chappie.srt", function(err, sub2) {
+        sub1 = toBinary(parseSubtitle(sub1), 1000);
+        sub2 = toBinary(parseSubtitle(sub2), 1000);
+        result = calculateOverlap(sub1, sub2);
+        console.log(result);
+    });
 });
 
-function compare() {
+function compare(longer, shorter, offset) {
+    var result = [];
+    for (var i=0; i<offset; i++) {
+        result[i] = 0;
+    }
+    for (var i=0; i<longer.length; i++) {
+        result[i+offset] = longer[i+offset] & shorter[i];
+    }
 
+    return result;
+}
+
+function calculateOverlap(sub1, sub2) {
+    var longer, shorter;
+    if (sub1.length > sub2.length) {
+        longer = sub1;
+        shorter = sub2;
+    } else {
+        longer = sub2;
+        shorter = sub1;
+    }
+
+    var least = Math.min(calculateLines(shorter), calculateLines(longer));
+    var best = 1;
+    var offset = -1;
+    for (var i=0; shorter.length+i<=longer.length; i++) {
+        var v = calculateLines(compare(longer, shorter, i));
+        v /= least;
+        if (v < best) {
+            best = v;
+            offset = i;
+        }
+        console.log({overlap: v, offset: i});
+    }
+
+    return {overlap: best, offset: offset};
+}
+
+function calculateLines(lines) {
+    var line = 0;
+
+    lines.forEach(function(interval) {
+        if (interval !== 0) {
+            line -= 1;
+        }
+        while (interval >= 1) {
+            if (interval % 2 !== 0) {
+                line++;
+            }
+            interval = interval >> 1;
+        }
+    });
+
+    return line;
 }
 
 function parseSubtitle(subtitle) {
@@ -18,9 +76,6 @@ function parseSubtitle(subtitle) {
 
 	lines.some(function(line) {
 		line = line.trim();
-		if (line === "Elin! stop here!") {
-			return true;
-		}
 		if (correctLine) {
 			correctLine = false;
 			intervals.push(parseInterval(line));
@@ -46,7 +101,7 @@ function parseInterval(interval) {
 
 function parseTime(time) {
     time = time.replace(',', ':');
-	var parsed = (Date.parse(DATE + " " + time) + 60*60*1000)/1000;
+	var parsed = (Date.parse(DATE + " " + time) + 60*60*1000);
     return parsed;
 }
 
@@ -71,10 +126,10 @@ function toBinary(intervals, bitLength) {
 function addToBinary(binary, length, value) {
 	var binaryLength = Math.ceil(Math.log2(binary + 1));
 
-	if (length + binaryLength <= 30) {
+	if (length + binaryLength <= BITS_PER_POSITION) {
 		return [bitShift(binary, length, value)];
 	} else {
-		var spaceLeft = 30 - binaryLength;
+		var spaceLeft = BITS_PER_POSITION - binaryLength;
 		var newBinaries = [bitShift(binary, spaceLeft, value)];
         return newBinaries.concat(addToBinary(1, length-spaceLeft, value));
 	}
