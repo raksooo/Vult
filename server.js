ed74c055b3e3c9394cb0ae2eb329738480275393
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2015-05-16 11:03:48 +0200

    added app.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var http = require('http');
 var mysql = require('mysql');
 var app = express();
+var xml2js = require('xml2js');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -16,56 +17,6 @@ var server = app.listen(8888, function () {
 
 });
 
-login();
 
-
-function login() {
-  var xmlData = "<methodCall>" +
-    "<methodName>LogIn</methodName>" +
-    "<params>" +
-    "<param>" +
-    "<value><string></string></value>" +
-    "</param>" +
-    "<param>" +
-    "<value><string></string></value>" +
-    "</param>" +
-    "<param>" +
-    "<value><string>en</string></value>" +
-    "</param>" +
-    "<param>" +
-    "<value><string>OS Test User Agent</string></value>" +
-    "</param>" +
-    "</params>" +
-    "</methodCall>";
-  var xmlTest = "<methodCall>" +
-    "<methodName>ServerInfo</methodName>" +
-    "</methodCall>";
-  console.log(xmlTest);
-
-
-
-  var postRequest = {
-    host: 'api.opensubtitles.org',
-    port: 80,
-    path: "/xml-rpc",
-    method: 'POST',
-    headers: {
-        'Content-Type': 'text/xml',
-        'Content-Length': Buffer.byteLength(xmlTest)
-    }
-  };
-
-  var buffer = '';
-  var req = http.request( postRequest, function( res )    {
-
-    console.log(res);
-    console.log( res.statusCode );
-    buffer = "";
-    res.on( "data", function( data ) { buffer = buffer + data; } );
-    res.on( "end", function( data ) { console.log( buffer ); } );
-  });
-
-  // req.write(xmlTest);
-  // req.end();
 }
 
