dea2af719b2a0297aba9b5e1adf8413c84c06093
Oskar Nyberg <oskar@oskarnyberg.com>
2015-05-16 17:58:06 +0200

    require

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mysql = require('mysql');
 var alg = require('./algorithm');
+var alg = require('./movies');
 var app = express();
 
 app.get('/', function (req, res) {
